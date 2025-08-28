1e5f8b6317af74db5338bfb84a6991ee023b9f05
Andrea <andreejaraguti@gmail.com>
2025-08-28 16:33:16 -0500

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/medescuento-renewed-web/', // 👈 IMPORTANTE para GitHub Pages
   server: {
     host: "::",
     port: 8080,
